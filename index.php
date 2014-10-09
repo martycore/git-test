cbd332157f31f7afb89dd075369681652e7a1b14
Marty <marty.race@gmail.com>
2014-10-09 13:06:53 +0100

    Added file to test

---
+++ index.php
@@ -0,0 +1,149 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- styles -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Josefin+Sans:100,300,400' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" href="http://www.martycore.co.uk/favicon.ico" type="image/vnd.microsoft.icon" />
+    
+    <link href="favicon.ico" rel="shortcut icon" type="" />  
+    
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <style type="text/css"></style>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    
+	</head>
+	
+	<body>
+		
+		<div class="wrapper">
+		
+			<div class="brand-wrapper">
+				<div class="brand">
+					<nav class="navbar navbar-default" role="navigation">
+						<div class="container-fluid">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+							
+								<div class="logo">
+									<a href="../">
+										<img src="images/martycore-logo.png" />
+									</a>
+								</div>
+							</div>
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<ul class="nav navbar-nav  navbar-right">
+								<li><a href="../">Home</a></li>
+								<li><a href="/about-martycorewebdev.php">About</a></li>
+								<li><a href="/web-development-services.php">Services</a></li>
+								<li><a href="/examples-of-work.php">Portfolio</a></li>
+								<li><a href="/blog.php">Blog</a></li>
+								<li><a href="/contact.php">Contact</a></li>
+							</ul>
+						</div><!-- /.container-fluid -->
+					</nav>
+				</div>
+			</div>
+			
+			<div class="banner-wrapper">
+				<div class="banner">
+					<img src="" />
+				</div>
+			</div>
+			
+			<div class="main-wrapper">
+				<div class="main-copy">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-xs-12">
+								<h1>Martycore Web Development</h1>
+								<hr />
+								<p>Welcome to the home of your friendly neighbourhood web development specialist! I offer hand tailored services for all your online needs; from bespoke website development to Wordpress, Drupal, Magento or Shopify site builds. My fresh, innovative ideas and solutions are just what you need to get your project off the ground.</p>
+							</div>
+						</div>
+					</div>
+				
+			
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-xs-12 col-sm-6">
+								<h2>100% Transparency</h2>
+								<hr />
+								<p>My services are specific and my costs transparent. There are no hidden costs or nasty surprises, each package details exactly what work I will complete for you and a timeframe in which I can have it done.</p>
+								<p>Bronze, Silver and Gold packages are available starting from £400 for builds of Wordpress websites, Magento or Shopify ecommerce websites and personalised, bespoke websites.</p>
+								<p>I cover domain names, hosting and other set-up costs within these packages so there is nothing additional to pay!</p>
+							</div>
+							<div class="col-xs-12 col-sm-6">
+							</div>
+						</div>
+					</div>
+			
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-xs-12 col-sm-6">
+								<h2>The After Party</h2>
+								<hr />
+								<p>After your website build has been completed and snagged to perfection, I offer three distinct maintenance packages. These packages range from content management to my full SEO service, meaning there is something for every site and budget.</p>
+								<p>I am here to help; any problems or support needed after the project will be addressed by me and fixed for free!</p>
+							</div>
+							<div class="col-xs-12 col-sm-6">
+							</div>			
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="footer-wrapper">
+				<div class="footer-copy">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-xs-12 col-sm-4">
+								<h6>Follow Me</h6>
+								<hr />
+								<div class="social">
+									<a href="www.facebook.com">
+									<img src="images/social/white-facebook-32.png" />
+									</a>
+									<a href="www.twitter.com">
+									<img src="images/social/white-twitter-32.png" />
+									</a>
+									<a href="www.instagram.com">
+									<img src="images/social/white-instagram-32.png" />
+									</a>
+									<a href="www.google.com">
+									<img src="images/social/white-google-32.png" />
+									</a>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-4">
+							</div>
+							<div class="col-xs-12 col-sm-4">
+								<div class="contact">
+									<h6>Talk To Me</h6>
+									<hr />
+									<a href="mailto:dev.martycore.com">
+										<p>e - dev@martycore.com</p>
+									</a>
+									<p>t - 07768769502</p>
+								</div>
+							</div> 
+						</div>
+					</div>
+				</div>
+			</div>
+		
+		</div>
+		
+	</body>
+
+</html>
